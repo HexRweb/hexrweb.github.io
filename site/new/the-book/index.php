799a6441ec1c22d5ea66accd004d3b3fde0b3f9d
Vikas <potlurivikas@gmail.com>
2016-09-01 16:05:35 -0500

    Lots of changes

---
+++ site/new/the-book/index.php
@@ -0,0 +1,11 @@
+---
+page.title = "The Book"
+---
+<div class="container">
+    <h1 class="center">The Book (by HexR Web)</h1>
+    <h3 class="center">Table of Contents</h3>
+    <ul class="center">
+        <li class="flow-text"><a href="/the-book/jan-2016-introduction">January 2016 (Introduction)</a></li>
+        <li class="flow-text"><a href="feb-2016-computer-components">February 2016 (Computer Components)</a></li>
+    </ul>
+</div>
