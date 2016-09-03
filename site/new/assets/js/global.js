a3991581704e1c55aadf50ab69be3bb501580cff
Vikas <vikaspotluri123@gmail.com>
2016-09-02 22:38:36 -0500

    This is the old version of the site

--- site/new/assets/js/global.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(".button-collapse").sideNav();$(".parallax").parallax();$('.modal-trigger').leanModal();$("html").niceScroll({scrollspeed: 120,mousescrollstep: 80,cursorwidth:8,zindex:999999});$(window).trigger("scroll");});
--- site/new/assets/js/projects.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(".project-coming-soon").hover(function(){$(this).find(".project-coming-soon-ribbon").fadeIn()},function(){$(this).find(".project-coming-soon-ribbon").hide()}),$(".project-logo").click(function(){null!==$(this).attr("data-bind-href")&&window.open($(this).attr("data-bind-href"))})});
