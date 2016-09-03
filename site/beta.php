42b4a548398f5a11922f9fec5c322e70b57866da
Vikas <vikaspotluri123@gmail.com>
2016-09-02 22:39:59 -0500

    Push new version of site

--- site/beta.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$path = isset($_GET['path']) ? $_GET['path'] : "";
-
-filter_var($path, FILTER_SANITIZE_URL);
-
-header("Location:http://preview.vl7kaitfbt9wjq307oeitrfi5293zousrcpeklnmi.box.codeanywhere.com/$path ");
-
-?>
